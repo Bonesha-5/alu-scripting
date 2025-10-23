6dbde23d676246c9e0d434e56caddcf54d922b41
Bonesha-5 <u.kevine@alustudent.com>
2025-10-23 10:32:19 +0200

    ok

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Module that queries the Reddit API and returns the number of subscribers."""
+"""
+Module that queries the Reddit API and returns the number of subscribers.
+"""
 import requests
 
 
