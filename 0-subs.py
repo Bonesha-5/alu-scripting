338860d05df2aee1b29e362fb3c5d2385351423e
Bonesha-5 <u.kevine@alustudent.com>
2025-10-23 08:57:04 +0000

    gg

---
+++ 0-subs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module to query Reddit API for subreddit subscriber counts
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries Reddit API and returns number of subscribers for a given subreddit
+
+    Args:
+        subreddit (str): The subreddit to query
+
+    Returns:
+        int: Number of subscribers, 0 if invalid subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'kevine-scripting-api-advanced'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data', {}).get('subscribers', 0)
+    return 0
