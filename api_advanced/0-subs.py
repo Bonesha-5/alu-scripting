b2260f0bde90d0ace8cd32fdf588cb107af93b83
Bonesha-5 <u.kevine@alustudent.com>
2025-10-23 10:13:12 +0200

    done

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""Module for querying the Reddit API to get subreddit subscriber count."""
+"""Module that queries the Reddit API and returns the number of subscribers."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return total number of subscribers for a given subreddit"""
+    """Return the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'keviScript/0.1'}
+    headers = {'User-Agent': 'MyRedditApp/0.1'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         return 0
 
-    data = response.json().get("data")
-    if data:
-        return data.get("subscribers", 0)
-    return 0
+    sub = response.json().get("data", {}).get("subscribers", 0)
+    return sub
